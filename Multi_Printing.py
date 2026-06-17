fee0ff22370b9a0ae821908d4a655560d52331de
NUKASANI VENKATA VINOD <vinodnukasani58@gmail.com>
2026-06-17 10:52:28 +0530

    Multi_Printing.py

---
+++ Multi_Printing.py
@@ -0,0 +1,14 @@
+# Given two inputs that are stored in variables a and n, you need to print a, n times in a single line without space between them. Output must have a newline at the end.
+
+# Examples:
+
+# Input: a = "Hello", n = 5
+# Output: HelloHelloHelloHelloHello
+# Explanation: a is printed n=5 times in a single line without space between them
+
+#Answer
+
+a = input()
+n = int(input())
+#code here
+print(n*a)
