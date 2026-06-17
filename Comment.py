7e263a8cd37a13f73d6a2f2e0f5cf90cd79ab2ef
NUKASANI VENKATA VINOD <vinodnukasani58@gmail.com>
2026-06-17 10:47:32 +0530

    Comment.py

---
+++ Comment.py
@@ -0,0 +1,28 @@
+# Given three lines that print the values of a, b, and c. Comment out the line that prints the value of b.
+
+# Comments are useful in any programming language to describe the purpose of functions or operations. They are ignored by the compiler, so they do not affect the execution of the code.
+
+# Examples: 
+
+# Input: a = 5, b = 6, c = 15
+# Output:
+# 5
+# 15
+# Input:a = 6, b = 8, c = 10
+# Output:
+# 6
+
+#Answer
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Prints a
+print(a)
+
+# Prints b
+#print(b)
+
+# Prints c
+print(c)
